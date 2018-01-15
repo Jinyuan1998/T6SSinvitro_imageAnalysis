653772e1af0f91598901860ef05f1e63b10cf56e
Jinyuan1998 <Jinyuan1998@users.noreply.github.com>
2018-01-15 17:07:30 -0500

    Update TimeLapseCellKilling.m

--- TimeLapseCellKilling.m
+++ TimeLapseCellKilling.m
@@ -1,9 +1,24 @@
 function T = TimeLapseCellKilling(folderName, useI, printH)
+%%%%% **********************************************************************************%%%%% 
+%%%%%  image analysis of time lapse using 63x oil, fluorescent cells.                   %%%%% 
+%%%%%  bpass function is from the track package by David Grier, John                    %%%%%
+%%%%%  Crocker, and Eric Weeks.                                                         %%%%%
+%%%%%  (https://site.physics.georgetown.edu/matlab/code.html)                           %%%%%
+%%%%% **********************************************************************************%%%%%
+
+%%%%% **********************************************************************************%%%%% 
+%%%%%  This matlab script is used to track Aeromonas veronii cell killed                %%%%% 
+%%%%%  by the type VI secretion system of Vibrio cholerae in vitro.                     %%%%%
+%%%%%  https://www.biorxiv.org/content/early/2017/11/29/226472.full.pdf+html            %%%%%
+%%%%% **********************************************************************************%%%%%
+
+%%% to do: the predator track still needs work due to weak fluoresent
+
 % folderName: the directory containing time lapse image
 % useI: the index of images used for analysis
 % printH: handle controls whether to print image or not; default is 0=not print; 1=print
 
-printH = 0;
+
 cd(folderName)
 f = dir('Experiment*');
 F = f(find(vertcat(f.isdir)));
@@ -60,7 +75,7 @@ for i=useimage
 end
 X(X~=0) 
 Y(Y ~= 0)
-% save('tform.mat', 'X', 'Y')
+
 %% 3. crop drifted images
 X1 = sum(X(X>=0));
 X2 = sum(X(X<0));
@@ -103,7 +118,6 @@ for i=1:length(useimage)
     % use watershed algorith %%%%%%%%%%  
     prbw = im2bw(imadjust(prb));
     prbw = bwareaopen(prbw, round(60/(1+i/10)));
-%     figure, imshow(prbw)
     L = watershed(prbw);
     L(~prbw) = 0;
     B = uint16(L) .* pr;
@@ -114,8 +128,6 @@ for i=1:length(useimage)
         figure; imagesc(imadjust(pr))
         figure; imshow(PRrgb)
     end
-
-%     prkw = pkfnd(prbw, 0.01, 11);
     prkw = pkfnd(B, 0.01, 11);
     prcnt = cntrd(double(Lbb), prkw, 4);
     warning off
@@ -125,16 +137,10 @@ for i=1:length(useimage)
     
     %%%%%%%%%%%%%%%%%%%%%%%%% analyze predator (not working well for now due to the weak fluorescence) %%%%%%%%%%%%%%%%
     pd = imread(['cropPd/' ctpdtimagefiles{i}]);
-%     p = imadjust(pd) - imadjust(pr);
-%     pd(pd<0) = 0;
     pdb = bpass(pd, 1, 4, median(median(pd))/(200-useimage(i)))-prb;
     pdb(pdb<0) = 0;
     pdbw = im2bw(imadjust(pdb));
-%     p = pdbw-prbw;
-    
     pdbw = bwareaopen(pdbw, 40);
-%     figure, imagesc(imadjust(pdb))
-%     figure, imshow(pdbw)
     
     pdL = watershed(pdbw, 4);
     pdL(~pdbw) = 0;
@@ -142,63 +148,27 @@ for i=1:length(useimage)
     cc = bwconncomp(C);
     Lcc = labelmatrix(cc);
     PDrgb = label2rgb(Lcc,'spring','k','shuffle');
-    if printFigure == 1
-        figure; imagesc(imadjust(pd))
-        figure; imshow(PDrgb)
-    end
 
     pdkw = pkfnd(pdbw, 0.01, 10);
     pdcnt = cntrd(pdbw,pdkw,11);
     pdpos_list = pdcnt(:,1:2);
     pdcountws(i) = size(pdpos_list, 1);
     if printFigure == 1
+        figure; imagesc(imadjust(pd))
+        figure; imshow(PDrgb)
         figure, imshowpair(prbw, pdbw, 'Scaling','joint'); 
-        hold on; plot(pdpos_list(:,1), pdpos_list(:,2), 'yx');
-    %     hold on; plot(prpos_list(:,1), prpos_list(:,2), 'r*')
-%         hold on; plot(pdkw(:,1), pdkw(:,2), 'y*')
+%         hold on; plot(pdpos_list(:,1), pdpos_list(:,2), 'yx');
         hold on; plot(prkw(:,1), prkw(:,2), 'r*')
         hold off
-
-    %     figure, imshowpair(imadjust(prb), imadjust(pdb), 'Scaling','joint'); 
-    %     hold on; plot(pdpos_list(:,1), pdpos_list(:,2), 'y*');
-    %     hold on; plot(prpos_list(:,1), prpos_list(:,2), 'r*')
     end
-    figure
-    imshow(imadjust(pr));
-    hold on; plot(prpos_list(:,1), prpos_list(:,2), 'rx', 'MarkerSize', 8);
-    hold off
-    print(['prcellplot/' ctpreyimagefiles{i}], '-dtiff')
-    close
+%     figure
+%     imshow(imadjust(pr));
+%     hold on; plot(prpos_list(:,1), prpos_list(:,2), 'rx', 'MarkerSize', 8);
+%     hold off
+%     print(['prcellplot/' ctpreyimagefiles{i}], '-dtiff')
+%     close
         
 end
 
 A = [useimage' prcountws' pdcountws'];
 T = array2table(A, 'variableNames', {'imageN' 'prey_N' 'predator_N'});
-
-%%
-T2 = T(1:length(useimage),:);
-
-% figure
-% plot(useimage/6, T2.prey_N, 'r*')
-% title('Number of Prey cells', 'FontSize', 16)
-% xlabel('time (h)', 'fontSize', 14)
-% ylabel('number of prey cells', 'fontSize',14)
-% figure
-% plot(T2.imageN/6, T2.predator_N, 'm*')
-% title('Number of Predator cell', 'FontSize', 16)
-% xlabel('time (h)', 'fontSize', 14)
-% ylabel('number of predator cells', 'fontSize',14)
-
-figure
-plot(T2.imageN/6, T2.prey_N/max(T2.prey_N) * 100, 'r*')
-title('Survival of Prey cells (%)', 'FontSize', 16)
-xlabel('time (h)', 'fontSize', 14)
-ylabel('percentage of prey cells', 'fontSize',14)
-set(gca, 'ylim', [0 110], 'ytick', 0:20:120)
-% figure
-% plot(T2.imageN/6, T2.predator_N/max(T2.predator_N) * 100, 'm*')
-% title('Survival of Predator cell (%)', 'FontSize', 16)
-% xlabel('time (h)', 'fontSize', 14)
-% ylabel('percentage of predator cells', 'fontSize',14)
-% set(gca, 'ylim', [0 110], 'ytick', 0:20:120)
-%%
